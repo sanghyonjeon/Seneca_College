16ed7d817ac2bc1df1cab166dce18609d9af4cf6
sanghyonjeon <shjeon5@myseneca.ca>
2023-01-20 21:58:51 -0500

    Completed w1p1 files

--- WS01/w1p1/50Best.cpp
+++ WS01/w1p1/50Best.cpp
@@ -1,3 +1,18 @@
+/*
+*****************************************************************************
+                          Workshop - #1 (Part-1)
+Full Name  : Sang Hyon Jeon
+Student ID#: 1123552194
+Email      : shjeon5@myseneca.ca
+Section    : NBB
+
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my
+professor provided to complete my workshops and assignments.
+*****************************************************************************
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include "Movie.h"
 using namespace std;
--- WS01/w1p1/Movie.h
+++ WS01/w1p1/Movie.h
@@ -1,18 +1,32 @@
-#ifndef NAMESPACE_MOVIE_H // replace NAMESPACE and HEADERFILENAME with relevant names
-#define NAMESPACE_MOVIE_H
+/*
+*****************************************************************************
+						  Workshop - #1 (Part-1)
+Full Name  : Sang Hyon Jeon
+Student ID#: 1123552194
+Email      : shjeon5@myseneca.ca
+Section    : NBB
 
-struct Movie {
-	char m_title[128];
-	int m_year;
-	char m_rating[6];
-	int m_duration;
-	char m_genres[10][11];
-	float m_consumerRating;
-};
+Authenticity Declaration:
+I have done all the coding by myself and only copied the code that my
+professor provided to complete my workshops and assignments.
+*****************************************************************************
+*/
 
-bool loadMovies();
-bool hasGenre(const Movie* mvp, const char genre[]);
-void displayMovie(const Movie* mvp);
-void displayMoviesWithGenre(const char genre[]);
+#ifndef SDDS_MOVIE_H
+#define SDDS_MOVIE_H
+namespace sdds {
+	struct Movie {
+		char m_title[128];
+		int m_year;
+		char m_rating[6];
+		int m_duration;
+		char m_genres[10][11];
+		float m_consumerRating;
+	};
 
+	bool loadMovies();
+	bool hasGenre(const Movie* mvp, const char genre[]);
+	void displayMovie(const Movie* mvp);
+	void displayMoviesWithGenre(const char genre[]);
+}
 #endif
