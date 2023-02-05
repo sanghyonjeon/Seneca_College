e965b7bf2eb62fdc50020d0f4c10f69dc4d0367a
sanghyonjeon <shjeon5@myseneca.ca>
2023-02-05 06:54:26 -0500

    updating a3 files

--- blog-service.js
+++ blog-service.js
@@ -41,4 +41,52 @@ function getCategories() {
   });
 }
 
-module.exports = { initialize, getAllPosts, getPublishedPosts, getCategories };
+function addPost(postData) {
+  return new Promise((resolve, reject) => {
+    if (postData.published === undefined) {
+      postData.published = false;
+    } else {
+      postData.published = true;
+    }
+    postData.id = posts.length + 1;
+    posts.push(postData);
+    resolve(postData);
+  });
+}
+
+function getPostsByCategory(category) {
+  return new Promise((resolve, reject) => {
+    var postsByCategory = posts.filter((post) => post.category === category);
+    if (postsByCategory.length === 0) return reject("no results returned");
+    resolve(postsByCategory);
+  });
+}
+
+function getPostsByMinDate(minDateStr) {
+  return new Promise((resolve, reject) => {
+    var postsByMinDate = posts.filter(
+      (post) => new Date(post.postDate) >= new Date(minDateStr)
+    );
+    if (postsByMinDate.length === 0) return reject("no results returned");
+    resolve(postsByMinDate);
+  });
+}
+
+function getPostById(id) {
+  return new Promise((resolve, reject) => {
+    var post = posts.find((post) => post.id === id);
+    if (!post) return reject("no result returned");
+    resolve(post);
+  });
+}
+
+module.exports = {
+  initialize,
+  getAllPosts,
+  getPublishedPosts,
+  getCategories,
+  addPost,
+  getPostsByCategory,
+  getPostsByMinDate,
+  getPostById,
+};
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /******************************************************************************
-                             WEB322 – Assignment 2
+                             WEB322 – Assignment 3
 Full Name  : Sang Hyon Jeon
 Student ID#: 1123552194
 Email      : shjeon5@myseneca.ca
-Section    : NBB
-Date       : February 3, 2023
+Section    : NGG
+Date       : February 17, 2023
 
 Authenticity Declaration:
 I have done all the coding by myself and only copied the code that my
@@ -14,9 +14,23 @@ Online (Cyclic) Link:
 ******************************************************************************/
 var blogService = require("./blog-service.js");
 var path = require("path");
+
 // The server must make use of the "express" module
 var express = require("express");
 var app = express();
+
+const multer = require("multer");
+const upload = multer(); // no { storage: storage } since we are not using disk storage
+
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: "dpju16bxg",
+  api_key: "528451683481667",
+  api_secret: "gU9teO1Y7g3BGj-yisQdph-xEag",
+  secure: true,
+});
+
+const streamifier = require("streamifier");
 
 // The server must listen on process.env.PORT || 8080
 var HTTP_PORT = process.env.PORT || 8080;
@@ -47,10 +61,46 @@ app.get("/blog", (req, res) => {
     });
 });
 
-// This route "/posts" gets all posts within posts.json
+// This route "/posts" gets posts within posts.json by category, minDate, or just all the posts
 app.get("/posts", (req, res) => {
+  let category = req.query.category;
+  let minDate = req.query.minDate;
+
+  if (category) {
+    blogService
+      .getPostsByCategory(category)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json({ message: err });
+      });
+  } else if (minDate) {
+    blogService
+      .getPostsByMinDate(minDate)
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json({ message: err });
+      });
+  } else {
+    blogService
+      .getAllPosts()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json({ message: err });
+      });
+  }
+});
+
+// This route will return a JSON formatted string containing a single post whose id matches the value
+app.get("/post/:id", (req, res) => {
+  const postId = req.params.id;
   blogService
-    .getAllPosts()
+    .getPostById(postId)
     .then((data) => {
       res.json(data);
     })
@@ -71,9 +121,45 @@ app.get("/categories", (req, res) => {
     });
 });
 
+// This route simply sends the file "/views/addPost.html"
+app.get("/posts/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "addPost.html"));
+});
+
 // Any other routes will return a custom message with an HTTP status code
 app.use((req, res) => {
   res.status(404).send("Page Not Found");
+});
+
+app.post("/posts/add", upload.single("featureImage"), (req, res) => {
+  let streamUpload = (req) => {
+    return new Promise((resolve, reject) => {
+      let stream = cloudinary.uploader.upload_stream((error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      });
+
+      streamifier.createReadStream(req.file.buffer).pipe(stream);
+    });
+  };
+
+  async function upload(req) {
+    let result = await streamUpload(req);
+    console.log(result);
+    return result;
+  }
+
+  upload(req).then((uploaded) => {
+    req.body.featureImage = uploaded.url;
+
+    // Process the req.body and add it as a new Blog Post before redirecting to /posts
+    blogService.addPost(req.body).then((postData) => {
+      res.redirect("/posts");
+    });
+  });
 });
 
 blogService
