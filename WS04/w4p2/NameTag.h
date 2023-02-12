a3aeb703d8b0d94327a487b4e9f50522ba02bd37
sanghyonjeon <shjeon5@myseneca.ca>
2023-02-12 05:21:02 -0500

    adding reflect.txt

--- WS04/w4p2/NameTag.h
+++ WS04/w4p2/NameTag.h
@@ -16,6 +16,36 @@ professor provided to complete my workshops and assignments.
 #include <iostream>
 
 namespace sdds {
+    const int MIN_NAME_LENGTH = 20;
+    const int MAX_NAME_LENGTH = 40;
 
+    class NameTag {
+        // Member Variables
+        char* m_name = nullptr;
+        int m_extension;
+        bool m_extension_exists;
+
+    public:
+        // Constructors & Destructor
+        NameTag();
+        NameTag(const char* name);
+        NameTag(const char* name, int extension);
+        ~NameTag();
+
+        // Member Functions
+        NameTag& read();
+        void print() const;
+
+        // Utility Functions
+        void setEmpty();
+        void emptyName();
+        void emptyExtension();
+
+        bool validName(const char* name);
+        bool validExtension(int extension);
+
+        void setName(const char* name);
+        void setExtension(int extension);
+    };
 }
 #endif // !SDDS_NAMETAG_H_
--- WS04/w4p2/main.cpp
+++ WS04/w4p2/main.cpp
@@ -23,9 +23,10 @@ int main() {
       {"Fred Soley", 12345} ,       // two argument constructor with good data
       {nullptr, 23456},    // two argument constructor with bad name
       {"Bad number", 1234},  // two argument constructor with bad number
-      {"Bad number", 123456}  // two argument constructor with bad number
-                             // default constructor (sixth element)
+      {"Bad number", 123456},  // two argument constructor with bad number
+                             {}// default constructor (sixth element)
    };
+
    int i;
    for (i = 0; i < 6; i++) {
       nt[i].print();
